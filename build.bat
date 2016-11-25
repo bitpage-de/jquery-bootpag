c5b862f392f9bb8d11bd3a3202aa85fa85fe88df
cpi <c.piontek@tk-world.de>
2016-11-25 18:32:38 +0100

    refactoring: files renamed.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 :: Using closure compiler (https://github.com/google/closure-compiler)
 :: http://dl.google.com/closure-compiler/compiler-latest.zip
-java -jar ../compiler-latest/compiler.jar --js lib/jquery.bootpag.js --js_output_file lib/jquery.bootpag.min.js
+java -jar closure-compiler-v20161024.jar --js lib/jquery.bootpag-advanced.js --js_output_file lib/jquery.bootpag-advanced.min.js
